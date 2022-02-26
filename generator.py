af08efb52d6646000e01773b2dffa49986778a0c
Sergio García Heredia <sergio.gh14@gmail.com>
2022-02-26 20:09:07 +0100

    feat: Add transpose convolution block

--- generator.py
+++ generator.py
@@ -3,6 +3,35 @@ from tensorflow.keras import models
 from tensorflow.keras import losses
 import numpy as np
 
+def transpose_conv_block(
+    x,
+    filters,
+    activation,
+    kernel_size=(3, 3),
+    strides=(1, 1),
+    padding="same",
+    use_bn=True,
+    use_bias=False,
+    use_dropout=False,
+    drop_value=0.3,
+):
+    x = layers.Conv2DTranspose(
+        filters,
+        kernel_size,
+        strides=strides,
+        padding=padding,
+        use_bias=use_bias
+    )(x)
+
+    if use_bn:
+        x = layers.BatchNormalization()(x)
+    if activation:
+        x = activation(x)
+    if use_dropout:
+        x = layers.Dropout(drop_value)(x)
+
+    return x
+
 
 def upsample_block(
     x,
@@ -19,7 +48,11 @@ def upsample_block(
 ):
     x = layers.UpSampling2D(up_size)(x)
     x = layers.Conv2D(
-        filters, kernel_size, strides=strides, padding=padding, use_bias=use_bias
+        filters,
+        kernel_size,
+        strides=strides,
+        padding=padding,
+        use_bias=use_bias
     )(x)
 
     if use_bn:
@@ -38,33 +71,13 @@ def get_generator(input_shape):
     x = layers.BatchNormalization()(x)
     x = layers.LeakyReLU(0.2)(x)
     x = layers.Reshape((4, 4, 256))(x)
-    x = upsample_block(
-        x,
-        128,
-        layers.LeakyReLU(0.2),
-    )
-    x = upsample_block(
-        x,
-        64,
-        layers.LeakyReLU(0.2),
-    )
-    x = upsample_block(
-        x,
-        32,
-        layers.LeakyReLU(0.2),
-    )
+    x = upsample_block(x, 128, layers.LeakyReLU(0.2))
+    x = upsample_block(x, 64, layers.LeakyReLU(0.2))
+    x = upsample_block(x, 32, layers.LeakyReLU(0.2))
     x = layers.Cropping2D((1, 1))(x)
-    x = upsample_block(
-        x,
-        16,
-        layers.LeakyReLU(0.2),
-    )
+    x = upsample_block(x, 16, layers.LeakyReLU(0.2))
     x = layers.Cropping2D((1, 1))(x)
-    x = upsample_block(
-        x,
-        1, 
-        layers.Activation("tanh"), 
-    )
+    x = upsample_block(x, 1, layers.Activation("tanh"))
     x = layers.Cropping2D((1, 1))(x)
 
     g_model = models.Model(z, x, name="generator")
