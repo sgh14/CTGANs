d713fb17e59103919ca4893348f30f1ea35e212e
Sergio García Heredia <sergio.gh14@gmail.com>
2022-03-08 16:58:04 +0100

    Update layers for MAGIC
    Now g_loss weights are an argument of get_generator_loss

--- generator.py
+++ generator.py
@@ -67,41 +67,41 @@ def upsample_block(
 
 def get_generator(input_shape):
     z = layers.Input(shape=input_shape)
-    x = layers.Dense(4 * 4 * 256, use_bias=False)(z)
+    x = layers.Dense(3 * 3 * 256, use_bias=False)(z)
     x = layers.BatchNormalization()(x)
     x = layers.LeakyReLU(0.2)(x)
-    x = layers.Reshape((4, 4, 256))(x)
+    x = layers.Reshape((3, 3, 256))(x)
+    # Upsample to (6, 6, 128)
     x = upsample_block(x, 128, layers.LeakyReLU(0.2))
+    # Upsample to (12, 12, 64)
     x = upsample_block(x, 64, layers.LeakyReLU(0.2))
+    # Upsample to (24, 24, 32)
     x = upsample_block(x, 32, layers.LeakyReLU(0.2))
-    # x = layers.Cropping2D((1, 1))(x)
-    # x = upsample_block(x, 16, layers.LeakyReLU(0.2))
-    # x = layers.Cropping2D((1, 1))(x)
-    x = upsample_block(x, 1, layers.LeakyReLU(0.2))#layers.Activation("tanh"))
-    # x = layers.Cropping2D((1, 1))(x)
+    # Upsample to (48, 48, 1)
+    x = upsample_block(x, 1, layers.LeakyReLU(0.2)) #layers.Activation("tanh"))
+    # Crop to (43, 43, 1)
+    x = layers.Cropping2D(((2, 3), (2, 3)))(x)
 
     g_model = models.Model(z, x, name="generator")
 
     return g_model
 
 
-def get_generator_loss(loss_name='basic'):
+def get_generator_loss(loss_name='basic', weights=np.array([1, 1, 1, 1])):
     if loss_name == 'basic':
         def generator_loss(disc_logits, pred_logits, disc_labels, pred_labels):
             bce = losses.BinaryCrossentropy(from_logits=True)
             cce = losses.CategoricalCrossentropy(from_logits=True)
             mae = losses.MeanAbsoluteError()
-            w = np.array([1, 0.3, 0.3, 0.3])
-            # TODO: use weights and allow tunning
-            loss = w[0]*bce(disc_labels, disc_logits)
+            loss = weights[0]*bce(disc_labels, disc_logits)
             if 'particletype' in pred_labels.keys():
-                loss += w[1]*cce(pred_labels['particletype'], pred_logits['particletype'])
+                loss += weights[1]*cce(pred_labels['particletype'], pred_logits['particletype'])
 
             if 'energy' in pred_labels.keys():
-                loss += w[2]*mae(pred_labels['energy'], pred_logits['energy'])
+                loss += weights[2]*mae(pred_labels['energy'], pred_logits['energy'])
 
             if 'direction' in pred_labels.keys():
-                loss += w[3]*mae(pred_labels['direction'], pred_logits['direction'])
+                loss += weights[3]*mae(pred_labels['direction'], pred_logits['direction'])
                      
             return loss
 
