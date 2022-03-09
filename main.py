e59fd61ce8aba223f58abe0ea2f8f3867f2b1788
Sergio García Heredia <sergio.gh14@gmail.com>
2022-03-09 22:22:49 +0100

    Create a custom callback Callback to periodically plot results and save the models, among other tasks

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #%%
 import tensorflow as tf
-from tensorflow.keras import models
-from tensorflow.keras import optimizers
+from tensorflow.keras import models, optimizers
 import os
 
 from data_loader import *
@@ -12,7 +11,6 @@ from GANs import *
 from data_generator import *
 
 #%% LOAD DATA
-# TODO: image normalization
 config_files_dir = 'config_files'
 GANs_config = os.path.join(config_files_dir, 'GANs.yml')
 dataset, data_features = load_data(GANs_config, 'train', batch_size=64, shuffle=False)
@@ -30,18 +28,10 @@ predictor = models.load_model(predictor_path)
 #%% BUILD THE GENERATOR
 generator_in_shape = (latent_dim + data_features['labels_dim'],)
 generator = get_generator(generator_in_shape)
-generator.summary()
-models_plots_dir = 'models_diagrams'
-os.makedirs(models_plots_dir, exist_ok=True)
-gen_plot_file = os.path.join(models_plots_dir, 'generator.png')
-tf.keras.utils.plot_model(generator, show_shapes=True, to_file=gen_plot_file)
 
 #%% BUILD THE DISCRIMINATOR
 discriminator_in_shape = data_features['image_shape']
 discriminator = get_discriminator(discriminator_in_shape)
-discriminator.summary()
-disc_plot_file = os.path.join(models_plots_dir, 'discriminator.png')
-tf.keras.utils.plot_model(discriminator, show_shapes=True, to_file=disc_plot_file)
 
 #%% BUILD GANS
 # Instantiate the GANs model.
@@ -62,7 +52,6 @@ discriminator_optimizer = optimizers.Adam(learning_rate=0.0002, beta_1=0.5, beta
 generator_loss = get_generator_loss()
 discriminator_loss = get_discriminator_loss()
 
-# TODO: add tensorboard callback
 # Compile the GANs model.
 gans.compile(
     d_optimizer=discriminator_optimizer,
@@ -72,20 +61,6 @@ gans.compile(
 )
 
 #%% TRAIN GANS
-# TODO: history = gans.fit() --> plot g_loss and d_loss evolution
-gans.fit(dataset, epochs=1, verbose=1)
-
-#%% SAVE THE MODELS
-generator_path = os.path.join(models_dir, 'generator.h5')
-discriminator_path = os.path.join(models_dir, 'discriminator.h5')
-generator.save(generator_path)
-discriminator.save(discriminator_path)
-
-#%% GENERATE DATA
-images_dir = 'images'
-os.makedirs(images_dir, exist_ok=True)
-features, labels = dataset.__getitem__(0)
-images = generate_data(latent_dim, labels)
-plot_grid(features['images'], save_path=os.path.join(images_dir, 'real_images.png'))
-plot_grid(images, save_path=os.path.join(images_dir, 'generated_images.png'))
-# %%
+# tensorboard = callbacks.TensorBoard('./logs', update_freq=1)
+plot_and_save = Plot_and_save(dataset)
+history = gans.fit(dataset, epochs=2, verbose=1, callbacks=[plot_and_save])
