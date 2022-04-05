2377f6ef1de89314e8cf782c62dc779466c1e46b
Sergio García Heredia <sergio.gh14@gmail.com>
2022-04-05 11:00:38 +0200

    Small aesthetic changes

--- data_generator.py
+++ data_generator.py
@@ -3,32 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# def plot_grid(images, nrows=5, save_path=''):
-#   channels = images.shape[-1]
-#   fig, axes = plt.subplots(nrows, channels, figsize=(channels*2, nrows*2), squeeze=False)
-#   for i in range(axes.shape[0]):
-#     for j in range(axes.shape[1]):
-#       axes[i, j].pcolor(np.squeeze(images[i, :, :, j]), cmap='viridis')
-#       axes[i, j].axis('off')
-      
-#   for j in range(axes.shape[1]):
-#     axes[0, j].set_title(f'Channel {j+1}')
-  
-#   if save_path:
-#     fig.savefig(save_path)
-
 def plot_grid(images, nrows=5, ncols=5, save_path=''):
   channels = images.shape[-1]
   fig = plt.figure(constrained_layout=True, figsize=(ncols*channels*2, nrows*2))
   subfigs = fig.subfigures(nrows=nrows, ncols=ncols, squeeze=False, wspace=0.1, hspace=0.1)
   for i in range(nrows):
     for j in range(ncols):
-      axes = subfigs[i, j].subplots(nrows=1, ncols=channels, squeeze=False)
+      axes = subfigs[i, j].subplots(nrows=1, ncols=channels, squeeze=False, gridspec_kw={'wspace': 0})
       axes = axes.flatten()
       for k in range(channels):
         axes[k].pcolor(np.squeeze(images[i*ncols+j, :, :, k]), cmap='viridis')
         axes[k].axis('off')
-        if i == 0:
+        if i == 0 and channels > 1:
           axes[k].set_title(f'Channel {k+1}')
   
   if save_path:
